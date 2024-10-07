883137b9f0c3c3a22602d8ee8a04b951931de577
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-07 11:37:05 +0800

    Local Query Scopes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +12,9 @@ class Book extends Model
 
     public function reviews() {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder {
+        return $query->where("title","LIKE","%". $title ."%");
     }
 }
