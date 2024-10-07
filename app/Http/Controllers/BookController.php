90698efd4b520013a34fb7537925c05481800224
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-07 15:32:49 +0800

    Rating and Review Count on All Pages

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -12,19 +12,20 @@ class BookController extends Controller
      */
     public function index(Request $request)
     {
-        $title = $request->input("title");
-        $filter = $request->input("filter", '');
-        $books = Book::when($title, fn($query, $title)=>$query->title($title));
+        $title = $request->input('title');
+        $filter = $request->input('filter', '');
+        $books = Book::when($title, fn ($query, $title) => $query->title($title));
         $books = match ($filter) {
             'popular_last_month' => $books->popularLastMonth(),
             'popular_last_6months' => $books->popularLast6Months(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
-            default => $books->latest()
+            default => $books->latest()->withAvgRating()->withReviewsCount()
         };
         // $books = $books->get();
-        $cacheKey = 'books' . $filter . ':' . $title;
-        $books = cache()->remember($cacheKey, 3600, fn()=> $books->get());
+        $cacheKey = 'books'.$filter.':'.$title;
+        // $books = cache()->remember($cacheKey, 3600, fn () => $books->get());
+        $books = $books->get();
 
         return view('books.index', ['books' => $books]);
     }
@@ -48,13 +49,13 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
-        $cacheKey = 'book:' . $book->id;
+        $cacheKey = 'book:'.$id;
 
-        $book = cache()->remember($cacheKey,3600, fn()=> $book->load(['reviews' => fn($query) => $query->latest()]));
+        $book = cache()->remember($cacheKey, 3600, fn () => Book::with(['reviews' => fn ($query) => $query->latest()])->withAvgRating()->withReviewsCount()->findOrFail($id));
 
-        return view('books.show', ['book'=>$book]);
+        return view('books.show', ['book' => $book]);
     }
 
     /**
