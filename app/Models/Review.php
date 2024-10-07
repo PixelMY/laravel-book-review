e36263a14437fe5764460dbd6331a3c5d2ad3080
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-07 11:24:51 +0800

    Querying & Associating Related Models

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['review', 'rating'];
     
     public function book() {
         return $this->belongsTo(Book::class);
