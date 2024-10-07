ebb15cfc93a29389c57146e66847d07ca03c7a05
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-07 16:34:10 +0800

    Blade Components - Star Rating Component

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -24,8 +24,8 @@ class BookController extends Controller
         };
         // $books = $books->get();
         $cacheKey = 'books'.$filter.':'.$title;
-        // $books = cache()->remember($cacheKey, 3600, fn () => $books->get());
-        $books = $books->get();
+        $books = cache()->remember($cacheKey, 3600, fn () => $books->get());
+        // $books = $books->get();
 
         return view('books.index', ['books' => $books]);
     }
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -38,7 +38,9 @@
 							<span class="book-author">by {{ $book->author }}</span>
 						</div>
 						<div>
-							<div class="book-rating">{{ number_format($book->reviews_avg_rating, 1) }}</div>
+							<div class="book-rating">
+								<x-star-rating :rating="$book->reviews_avg_rating" />
+							</div>
 							<div class="book-review-count">out of {{ $book->reviews_count }}
 								{{ Str::plural("review", $book->reviews_count) }}
 							</div>
