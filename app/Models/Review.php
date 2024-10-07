e44516f39af79e271598af2806e67680188d01bf
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-07 14:59:51 +0800

    Invalidating Cache

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -14,4 +14,8 @@ class Review extends Model
     public function book() {
         return $this->belongsTo(Book::class);
     }
+    protected static function booted() {
+        static::updated(fn(Review $review)=>cache()->forget('book:' . $review->book_id));
+        static::deleted(fn(Review $review)=>cache()->forget('book:' . $review->book_id));
+    }
 }
